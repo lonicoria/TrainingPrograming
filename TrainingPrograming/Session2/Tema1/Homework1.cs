0020ca1237a04a1f9f9d19a611783ca6dba4f8b1
Lorena Nicoria <lorena.nicoria@endava.com>
2024-12-03 14:50:47 +0200

    .

--- TrainingPrograming/Session2/Tema1/Homework1.cs
+++ TrainingPrograming/Session2/Tema1/Homework1.cs
@@ -4,59 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrainingPrograming.Session2.Tema1
+namespace TrainingPrograming.Session2
 {
-    // public class Homework1
-    // {
-    //  string firstName = "Lorena";
-    //  string lastName= "Nicoria";
-    //  int age = 39;
-    // int ageIn20Years = age + 20;
-    // string gender= "feminin";
-
-    //  [Test]
-    //public void practiceTest()
-    //  {
-    //    Console.WriteLine("Hello, " + "\n" + "Please input your First Name: " + "\n" + firstName);
-    //    Console.WriteLine();
-    //   Console.WriteLine("Please input your Last name: " + "\n" + lastName);
-    //   Console.WriteLine();
-    //   Console.WriteLine("Please input your age: " + "\n" + age);
-    //  Console.WriteLine();
-    //  Console.WriteLine("Please input your gender: " + "\n" + gender);
-
-    public class Homework
+    public class Homework1
     {
-        [SetUp]
-        public void Setup()
+        string firstName = "Lorena";
+        string lastName = "Nicoria";
+        int age = 39;
+        string gender = "Female";
+
+        public int AgeIn20Years
         {
+            get
+            {
+                return age + 20;
+            }
         }
 
         [Test]
-        public void Test1()
+        public void practiceTest()
         {
-            Person person = new()
-            {
-                FirstName = "Jane",
-                LastName = "Doe",
-                Age = 39,
-                Gender = "Female"
-            };
-
-            Console.WriteLine("\nYour details are as follows:");
-            Console.WriteLine("\t- First Name: " + person.FirstName);
-            Console.WriteLine("\t- Last Name: " + person.LastName);
-            Console.WriteLine("\t- Gender: " + person.Gender);
-            Console.WriteLine("\t- Age: " + person.Age);
-
-            var ageIn20Years = CalculateAgeIn20Years(person.Age);
-
-            Console.WriteLine("\nYou will be " + ageIn20Years + " years old in 20 years!");
+            Console.WriteLine("Hello, " + "\n" + "Please input your First Name: " + "\n" + firstName);
+            Console.WriteLine();
+            Console.WriteLine("Please input your Last name: " + "\n" + lastName);
+            Console.WriteLine();
+            Console.WriteLine("Please input your age: " + "\n" + age);
+            Console.WriteLine();
+            Console.WriteLine("Please input your gender: " + "\n" + gender);
         }
-
-        public static int CalculateAgeIn20Years(int currentAge)
-        {
-            return currentAge + 20;
-        }
+        
     }
 }
+       
