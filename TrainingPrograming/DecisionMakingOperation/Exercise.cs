4135b569cb183e5a9032b61a679a9be3e4356f11
Lorena Nicoria <lorena.nicoria@endava.com>
2024-12-18 16:13:48 +0200

    Tema DecisionOperations.

---
+++ TrainingPrograming/DecisionMakingOperation/Exercise.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPrograming.DecisionMakingOperation
+{
+    public class Exercise
+    {
+        [Test]
+        public void TestMethod()
+        {
+            //VerificareNumar(10, 25);
+            // VerificareNumar(21, 2);
+            CompareDigits(5);
+            //VerificareMasina("BMW");
+            VerificareMasina("Dacia");
+
+
+        }
+        public void VerificareNumar(int x, int y)
+        {
+
+            if (x < y)
+            {
+                Console.WriteLine($"Numarul {x} este mai mic decat numarul {y}");
+            }
+            else
+            {
+                Console.WriteLine($"Numarul {x} este mai mare decat numarul {y}");
+            }
+
+
+        }
+
+        public void CompareDigits(int x)
+        {
+
+            if (x < 5)
+            {
+                Console.WriteLine($"Numarul {x} este mai mic decat 5");
+
+            }
+            else if (x > 5)
+            {
+                Console.WriteLine($"Numarul {x} este mai mare decat 5");
+            }
+            else
+            {
+                Console.WriteLine($"Numarul {x} este egal cu 5");
+            }
+        }
+
+        public void VerificareMasina(string masina)
+        {
+            switch (masina)
+            {
+                case "Mercedes":
+                    Console.WriteLine("Mercedes Este disponibil in stoc");
+                    break;
+                case "Opel":
+                    Console.WriteLine("Opel Este disponibil in stoc");
+                    break;
+                case "BMW":
+                    Console.WriteLine("BMW Este disponibil in stoc");
+                    break;
+                default:
+                    Console.WriteLine("Marca nu este disponibila");
+                    break;
+
+
+            }
+        }
+    }
+}
+
+
---
+++ TrainingPrograming/DecisionMakingOperation/Tema_DecisionOperations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingPrograming.Session2.Metoda.HomeworkMetoda;
+
+namespace TrainingPrograming.DecisionMakingOperation
+{
+    public class Tema_DecisionOperations
+    {
+
+        public class Person
+        {
+            // Property to store the age of the person
+            public int Age { get; set; }
+            public string Gender { get; set; }
+
+
+            // Method to determine and output the age category
+            public void IsOld()
+            {
+                if (Age >= 100)
+                {
+                    Console.WriteLine("You are ancient!");
+                }
+                else if (Age >= 50)
+                {
+                    Console.WriteLine("You are old!");
+
+                }
+                else           //Age <50
+                {
+                    Console.WriteLine("You are still young!");
+                }
+            }
+
+            public void ShowGender()
+            {
+                switch (Gender)
+                {
+                    case "Male":
+                        Console.WriteLine("You are male!");
+                        break;
+                    case "Female":
+                        Console.WriteLine("You are female!");
+                        break;
+                    default:
+                        Console.WriteLine("You haven't specified your gender!");
+                        break;
+                }
+            }
+
+
+
+            [Test]
+
+            public void TestMethod()
+            {
+
+
+                Person person1 = new Person { Age = 150, Gender = "Male" };
+                person1.IsOld();
+                person1.ShowGender();
+
+                Person person2 = new Person { Age = 15, Gender = "Female" };
+                person2.IsOld();
+                person2.ShowGender();
+
+
+                Person person3 = new Person { Age = 55, Gender = "Not relevant" };
+                person3.IsOld();
+                person3.ShowGender();
+
+
+
+            }
+
+        }
+
+    }
+
+}       
+          
+
